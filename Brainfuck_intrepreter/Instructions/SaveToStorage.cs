8d7021ff7998b86165f5bb15d970cd0cbf051b56
Matěj Drábek <matej1drabek@seznam.cz>
2022-05-31 00:14:17 +0200

    Remove unused namespaces

--- Brainfuck_intrepreter/Instructions/SaveToStorage.cs
+++ Brainfuck_intrepreter/Instructions/SaveToStorage.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Brainfuck_intrepreter.Instructions
+﻿namespace Brainfuck_intrepreter.Instructions
 {
     internal class SaveToStorage : IInstruction
     {
